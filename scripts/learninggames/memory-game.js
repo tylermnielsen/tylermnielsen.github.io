f8763e389fadee4792ced99127c0370a8807cccc
Tyler Nielsen <tylernielsenn@gmail.com>
2022-05-04 14:27:56 -0400

    tweaks and started memory-game

---
+++ scripts/learninggames/memory-game.js
@@ -0,0 +1,54 @@
+const cardArray = [
+    {
+        name: 'fries',
+        img: 'images/memeoryGameImages/fries'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/memeoryGameImages/cheeseburger'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/memeoryGameImages/hotdog'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/memeoryGameImages/ice-cream'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/memeoryGameImages/milkshake'
+    },
+    {
+        name: 'pizza',
+        img: 'images/memeoryGameImages/pizza'
+    },
+    {
+        name: 'fries',
+        img: 'images/memeoryGameImages/fries'
+    },
+    {
+        name: 'cheeseburger',
+        img: 'images/memeoryGameImages/cheeseburger'
+    },
+    {
+        name: 'hotdog',
+        img: 'images/memeoryGameImages/hotdog'
+    },
+    {
+        name: 'ice-cream',
+        img: 'images/memeoryGameImages/ice-cream'
+    },
+    {
+        name: 'milkshake',
+        img: 'images/memeoryGameImages/milkshake'
+    },
+    {
+        name: 'pizza',
+        img: 'images/memeoryGameImages/pizza'
+    }
+]
+
+cardArray.sort(() => 0.5 - Math.random())
+
+console.log(cardArray)
