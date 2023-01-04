ddbd329f659958ba732e0b7ef5ddc2237f046102
SnailDragon <tylernielsenn@gmail.com>
2023-01-03 22:17:48 -0500

    better formatting before loading charts, preliminary wikidata queries for author genders

--- scripts/grstat.js
+++ scripts/grstat.js
@@ -57,13 +57,117 @@ function processData(raw){
 
 function authorGender(bookList){
 
-    //access wikidata
-            
+    //access wikidata    
+    const counts = new Map();
+
 
     const genderCounts = new Map();
-    bookList.forEach((book) => {
+    const problems = [];
 
-    });
+    var it = 0;
+    queries = setInterval(() => {
+        //console.log("working");
+        if(it >= bookList.length){
+            console.log("done");
+            console.log(counts);
+            console.log(problems);
+            clearInterval(queries);
+            fixProblems(counts, problems);
+        }
+        if(it < bookList.length) queryWQSData(clean(bookList[it].Author), counts, problems);
+        it++;
+
+    }, 1);
+
+    console.log(counts);
+}
+
+function fixProblems(counts, problems){
+    
+}
+
+ // let url = `https://en.wikipedia.org/w/api.php?origin=*&action=query&list=search&srsearch=${problems[it]}&format=json`;
+
+        // if(it >= problems.length){
+        //     console.log("done");
+        //     clearInterval(problemParse);
+        // }
+
+        // if(it < problems.length)
+        // fetch(url)
+        //     .then(function(response){return response.json();})
+        //     .then(function(response){
+        //         console.log("Name: " + it + " " + problems[it]);
+        //         console.log(response.);
+        //         it++;
+        //     })
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
+
+function clean(str){
+    //console.log(str);
+    for(var i = 0; i < str.length-1; i++){
+        if(str[i] == " " && str[i+1] == " "){
+            str = str.substring(0,i) + str.substring(i+1);
+            i--;
+        }
+        if(str[i] == "." && str[i+1] != " "){
+            str = str.substring(0,i+1) + " " + str.substring(i+1);
+        }
+    }
+    return str;
+}
+
+function queryWQSData(name, counts, problems){
+    let url = "https://query.wikidata.org/sparql?query=";
+    url += `SELECT * WHERE { ?person rdfs:label '${name}'@en; wdt:P31 wd:Q5; wdt:P21 ?gender . ?gender rdfs:label ?genderName filter(lang(?genderName) = "en") }`;
+    url += "&format=json";
+
+    return fetch(url)
+        .then(function(response){ return response.json();})
+        .then(function(response){
+            let gender;
+            try {
+                gender = response.results.bindings[0].genderName.value;
+            } catch(error) {
+                console.log("Error on " + name + " -> " + error);
+                problems.push(name);
+                gender = "error";
+            }
+            if(counts.has(gender)) counts.set(gender, counts.get(gender)+1);
+            else counts.set(gender, 1);
+        })
+        .catch((error) =>{
+            console.log(error);
+        });
+}
+
+function queryWikiData(name){
+    let url = "https://en.wikipedia.org/w/api.php"; 
+    const params = {
+        action: "query",
+        list: "search",
+        srsearch: name,
+        format: "json"
+    };
+
+    url = url + "?origin=*";
+    Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
+
+    fetch(url)
+        .then(function(response){return response.json();})
+        .then(function(response) {
+            for(var i = 0; i < 5; i++){
+                if(i >= response.query.search.size) break;
+                if(response.query.search[i].title === name){
+                    console.log("Yes");
+                    break;
+                }
+            }
+        })
+        .catch(function(error){console.log(error);});
+
 }
 
 function yearCounts(bookList){
